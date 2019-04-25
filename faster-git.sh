15430e4e2085e22b1c678eba2ecefd9979048381
Darlan Alves <darlan.alves@tnt-digital.com>
2019-04-25 14:32:56 +0200

    feat(*): add bash script for inclusion in .bashrc

---
+++ faster-git.sh
@@ -0,0 +1,40 @@
+killport() {
+  lsof -ti:$1 | xargs kill
+}
+
+alias gs="git status"
+alias grm="git fetch && git rebase origin/master"
+alias gbp="git push origin \$(git symbolic-ref --short HEAD) -u --no-verify"
+alias gfp="git add -A && git recommit && git push origin \$(git symbolic-ref --short HEAD) -u --no-verify -f"
+alias grc="git add -A && git rebase --continue"
+
+gb() {
+  if [ -z "$1" ]; then
+    git branch;
+  else
+    git checkout -b $1;
+  fi
+}
+
+gco() {
+  BRANCHES=`git for-each-ref --format="%(refname:short)" refs/heads | grep $1`
+  BRANCHES_COUNT=`git for-each-ref --format="%(refname:short)" refs/heads | grep $1 | wc -l`
+  if [ $BRANCHES_COUNT -gt 1 ]; then
+    echo "Too many matches:";
+    for branch in $BRANCHES; do echo "  $branch"; done
+    return 0
+  elif [ $BRANCHES_COUNT -eq 1 ]; then
+    git checkout $BRANCHES
+  else
+    echo "Not found"
+  fi
+}
+
+git_branch() {
+    git symbolic-ref --short HEAD 2> /dev/null
+}
+
+git_list_branches() {
+    git for-each-ref refs/heads --format='%(refname)' | cut -d/ -f3-
+}
+
